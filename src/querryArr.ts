b12fe081bc494062dc77fb535f1c1af2d8921b2d
wyatth7 <whardin1010@gmail.com>
2020-12-08 13:48:53 -0500

    Added documentation to querryArr.ts

--- src/querryArr.ts
+++ src/querryArr.ts
@@ -1,3 +1,9 @@
+/**
+ * @param arr String array to be quiried
+ * @param string What is used to query arr
+ *
+ * functionality: Takes a given string, and returns a new array of elements within 'arr' that include 'string'.
+ */
 export const querryArr = (arr: string[], string: string): string[] => {
   let newArr = arr.map((el) => {
     if (el.includes(string)) {
